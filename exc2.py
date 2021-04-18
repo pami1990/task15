2ded9e455c2977bf1c86095fc07a05af6f2dc761
pami1990 <pami1990pami@gmail.com>
2021-04-18 18:40:33 +0430

    git commit

--- exc2.py
+++ exc2.py
@@ -1,17 +1,16 @@
-#exc 2
+# exc 2
 
 def piglatin(str):
-
-      if str[0] == 'a' or str[0] == 'e' or str[0] == 'i' or str[0] == 'o':
-        str = str + 'way'
-      else:
+    if str[0] == 'a' or str[0] == 'e' or str[0] == 'i' or str[0] == 'o':
+        str1 = str + 'way'
+    else:
         x = range(str.__len__())
         for i in x:
-           if str[i] != 'a' or str[i] != 'e' or str[i] != 'i' or str[i] != 'o':
-             str.replace(str[i],'')
-        str = str + 'ay'
-
-      return str
+            if str[0] != 'a' and str[0] != 'e' and str[0] != 'i' and str[0] != 'o':
+               str = str.replace(str[0], '')
+               #print(str)
+        str1 = str + 'ay'
+    return str1
 
 
 sentence = input('pls enter your sentence : ')
@@ -19,5 +18,6 @@ words = sentence.split(' ')
 x = range(words.__len__())
 new_sen = ''
 for i in x:
-    new_sen = piglatin(words[i]) + ' '
+    new_sen = new_sen + ' ' + piglatin(words[i])
+
 print(new_sen)

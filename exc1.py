57a68feee691ad6c9196c0d17c52f4d5cfbab628
pami1990 <pami1990pami@gmail.com>
2021-04-20 16:45:21 +0430

    git commit

--- exc1.py
+++ exc1.py
@@ -12,8 +12,27 @@ for i in x:
     col1.append(row1[0])
     col2.append(row1[1])
     col3.append(row1[2])
-col1.sort()
-col2.sort()
-col3.sort()
+# col1.sort()
+# col2.sort()
+# col3.sort()
+def sort(col):
+   x = range(3)
+   for i in x:
+      if col[0] > col[1]:
+        a = col[0]
+        col[0] = col[1]
+        col[1] = a
+      if col[1]>col[2] :
+        a = col1[1]
+        col[1] = col[2]
+        col[2] = a
+      if col[0] > col[1]:
+        a = col1[0]
+        col[0] = col[1]
+        col[1] = a
+   return col
+sort(col1)
+sort(col2)
+sort(col3)
 print(col1[-1], col2[-1], col3[-1])
 print(col1, col2, col3)

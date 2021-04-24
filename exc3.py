928b84a30b45e1dfd873c6db10b771b043b322ce
pami1990 <pami1990pami@gmail.com>
2021-04-24 12:19:03 +0430

    git commit

--- exc3.py
+++ exc3.py
@@ -4,6 +4,15 @@ str = input('pls enter your math string : ')
 
 x = range(str.__len__())
 list1 = []
+num = ''
 for i in x:
-    list1.append(str[i])
+    if str[i] == ' ' or str[i] == '/' or str[i] == '+' or str[i] == '=' or str[i] == ',' or str[i] == '*' or str[i] == '-' or str[i] == '^':
+       if num != '':
+          list1.append(num)
+          num =''
+       list1.append(str[i])
+    else:
+        num += str[i]
+if num != '':
+ list1.append(num)
 print(list1)
